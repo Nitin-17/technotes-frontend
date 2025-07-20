8a39d8ab7d754b5da0456328549ac09861ba0e45
nitin-17 <saininitin945@gmail.com>
2025-07-21 01:24:52 +0530

    changed added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Login from "./features/auth/Login";
 import WelcomePage from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList";
-import RequireAuth from "./features/auth/requireAuth";
+import RequireAuth from "./features/auth/RequireAuth";
 import EditUser from "./features/users/EditUser";
 import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
